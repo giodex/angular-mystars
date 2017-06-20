eccc7b0141c1b1f7ca05f6e53224fa7c574425a2
giodex <deejaygas@gmail.com>
2017-06-20 11:25:12 +0200

    New version of js module

--- angular-mystars.js
+++ angular-mystars.js
@@ -1,52 +1,58 @@
-angular.module('angular-mystars').directive('starRating', function() {
-    return {
+var module = angular.module('angular-mystars');
 
-        restrict: 'A',
-        template: '<ul class="rating">' +
-            '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
-            '\u2605' +
-            '</li>' +
-            '</ul>',
-        scope: {
-            ratingValue: '=',
-            max: '=',
-            onRatingSelected: '&'
-        },
-        link: function(scope, elem, attrs) {
+module
+    .directive('starRating', function() {
+        'use strict';
 
-            var updateStars = function() {
-                scope.stars = [];
-                for (var i = 1; i < (scope.max + 1); i++) {
-                    console.info('valore di i ' + i);
-                    scope.stars.push({
-                        filled: i < (scope.ratingValue)
-                    });
-                }
-            };
+        return {
 
-            scope.toggle = function(index) {
-                console.info('index ' + index + 'scope ' + scope.ratingValue)
-                if (index == 0 && scope.ratingValue == 2) {
-                    alert('Non puoi dare 0 stelle dopo aver votato la prima volta!');
-                } else {
-                    scope.ratingValue = index + 2;
-                    scope.onRatingSelected({
-                        rating: index + 1
-                    });
-                }
-            };
+            restrict: 'A',
+            template: '<ul class="rating">' +
+                '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
+                '\u2605' +
+                '</li>' +
+                '</ul>',
+            scope: {
+                ratingValue: '=',
+                max: '=',
+                onRatingSelected: '&'
+            },
+            link: function(scope, elem, attrs) {
 
-            scope.$watch('ratingValue', function(oldVal, newVal) {
-                if (newVal) {
-                    updateStars();
-                }
-            });
+                var updateStars = function() {
+                    scope.stars = [];
+                    for (var i = 1; i < (scope.max + 1); i++) {
+                        console.info('valore di i ' + i);
+                        scope.stars.push({
+                            filled: i < (scope.ratingValue)
+                        });
+                    }
+                };
+
+                scope.toggle = function(index) {
+                    console.info('index ' + index + 'scope ' + scope.ratingValue)
+                    if (index == 0 && scope.ratingValue == 2) {
+                        alert('Non puoi dare 0 stelle dopo aver votato la prima volta!');
+                    } else {
+                        scope.ratingValue = index + 2;
+                        scope.onRatingSelected({
+                            rating: index + 1
+                        });
+                    }
+                };
+
+                scope.$watch('ratingValue', function(oldVal, newVal) {
+                    if (newVal) {
+                        updateStars();
+                    }
+                });
+            }
+
         }
 
-    }
-});
-angular.module('angular-mystars').controller("MyStarController", ['$scope', '$rootScope', '$location', '$window',
-    function($scope, $rootScope, $location, $window) {
+    })
+    .controller('MyStarController', ["$scope", "$timeout", function($scope) {
+        'use strict';
         $scope.rating = 0
         $scope.ratings = [{
             current: 1,
@@ -59,6 +65,4 @@ angular.module('angular-mystars').controller("MyStarController", ['$scope', '$ro
 
 
 
-
-    }
-]);
+    }]);
